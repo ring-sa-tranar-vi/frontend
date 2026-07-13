5e086cd5e52cbdec0b7ec904a6aaa20e20cf9958
Jakub Drewniak <jakub.t.drewniak@gmail.com>
2026-07-13 16:50:15 +0200

    implement faro tracing into root of proj

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,35 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './i18n'
+import {
+  faro,
+  getWebInstrumentations,
+  initializeFaro,
+  ReactIntegration,
+} from '@grafana/faro-react'
+import { TracingInstrumentation } from '@grafana/faro-web-tracing'
+
+initializeFaro({
+  url: 'https://faro-collector-prod-eu-north-0.grafana.net/collect/9e36dac7f625728f41392c8e7a905770',
+  app: {
+    name: 'ringsatranarvi-frontend',
+    version: '1.0.0',
+    environment: 'production',
+  },
+  instrumentations: [
+    ...getWebInstrumentations(),
+    new TracingInstrumentation(),
+    new ReactIntegration(),
+  ],
+})
 
 const router = createRouter({ routeTree })
 const queryClient = new QueryClient()
+
+router.subscribe('onResolved', (search) => {
+  const currentPath = search.toLocation.pathname
+  faro.api.setView({ name: currentPath })
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
